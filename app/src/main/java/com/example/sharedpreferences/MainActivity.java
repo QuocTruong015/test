3986b7961733133e6607160470e08f65c04720fb
QuocTruong015 <ngocquoc150604@gmail.com>
2024-10-21 21:41:02 +0700

    baitap

---
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.sharedpreferences;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnXacNhan;
+    EditText edtUsername, edtPassword;
+    CheckBox cbRemember;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        AnhXa();
+    }
+
+    private void AnhXa(){
+        btnXacNhan  = findViewById(R.id.button);
+        edtPassword = findViewById(R.id.editTextTextPassword);
+        edtUsername = findViewById(R.id.editTextText);
+        cbRemember  = findViewById(R.id.checkBox);
+    }
+
+}
